94714cb192956e5335d3e32be037edfabd5b8f57
AnushkaJoshi <anushkazinc@gmail.com>
2019-05-08 22:14:58 +0530

    frgments

--- EventManager/app/src/main/java/com/example/android/eventmanager/cognizance.java
+++ EventManager/app/src/main/java/com/example/android/eventmanager/cognizance.java
@@ -2,6 +2,7 @@ package com.example.android.eventmanager;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -15,21 +16,32 @@ public class cognizance extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment=null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                   fragment= new homecogn();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragment= new homecogn();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragment= new homecogn();
                     return true;
             }
             return false;
         }
     };
-
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +49,10 @@ public class cognizance extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new homecogn());
+
+        //getting bottom navigation view and attaching the listener
+
     }
 
 }
---
+++ EventManager/app/src/main/java/com/example/android/eventmanager/homecogn.java
@@ -0,0 +1,20 @@
+package com.example.android.eventmanager;
+
+import android.os.Bundle;
+import android.support.v4.app.BundleCompat;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import javax.annotation.Nullable;
+
+public class homecogn extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        return inflater.inflate(R.layout.fragment_homec, null);
+    }
+}
